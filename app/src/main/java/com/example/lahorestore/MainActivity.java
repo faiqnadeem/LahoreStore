adbb9e1443a7db30f1d99e7b2eb667f2bcb13474
Muhammad Faiq Nadeem <chfaiqnadeem@gmail.com>
2020-04-04 19:58:34 +0500

    LahoreStore app running with webview at default stage with welcome screen

---
+++ app/src/main/java/com/example/lahorestore/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.lahorestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+    private String webUrl = "https://lahorestore.pk";
+    ProgressBar progressBarWeb;
+    ProgressDialog progressDialog;
+    RelativeLayout relativeLayout;
+    Button button404;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+       // Window window = getWindow();
+       // window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        webview = (WebView) findViewById(R.id.web1);
+        progressBarWeb = (ProgressBar) findViewById(R.id.ProgressBar);
+        //progressDialog = new ProgressDialog(this);
+        //progressDialog.setMessage("Please Wait While Loading...");
+
+        button404 = (Button) findViewById(R.id.retry404);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+
+        checkConnection();
+
+        webview.loadUrl(webUrl);
+        webview.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        //webview.clearCache(true);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+
+        webview.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webview.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webview.getSettings().setAppCacheEnabled(true);
+        webview.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webview.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webview.getSettings().setUseWideViewPort(true);
+        webview.getSettings().setSavePassword(true);
+        webview.getSettings().setSaveFormData(true);
+        webview.getSettings().setEnableSmoothTransition(true);
+
+
+        webview.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBarWeb.setVisibility(View.VISIBLE);
+                progressBarWeb.setProgress(newProgress);
+                setTitle("Lahore Store is Under Development");
+                //progressDialog.show();
+
+                if(newProgress == 100){
+
+                    progressBarWeb.setVisibility(View.GONE);
+                    //setTitle(view.getTitle());
+                    //progressDialog.dismiss();
+                }
+
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+
+
+        button404.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkConnection();
+            }
+        });
+
+        }
+
+
+    @Override
+    public void onBackPressed() {
+        if(webview.canGoBack()){
+            webview.goBack();
+        }
+        else{
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("ARE YOU SURE YOU WANT TO EXIT ?")
+                    .setNegativeButton("NO",null)
+                    .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finishAffinity();
+                        }
+                    }).show();
+
+        }
+
+    }
+
+
+
+    public void checkConnection(){
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileNetwork = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if(wifi.isConnected()){
+
+            webview.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility((View.GONE));
+
+        }
+
+        else if (mobileNetwork.isConnected()){
+
+            webview.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility((View.GONE));
+        }
+
+        else{
+
+            webview.setVisibility(View.GONE);
+            relativeLayout.setVisibility((View.VISIBLE));
+
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/lahorestore/welcome.java
@@ -0,0 +1,42 @@
+package com.example.lahorestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Window window = getWindow();
+        //window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_welcome);
+
+        getSupportActionBar().hide();
+
+        Thread welcome = new Thread(){
+
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        welcome.start();
+
+
+    }
+}
